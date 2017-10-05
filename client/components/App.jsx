237aeacf80638b9372a2ddde920dc582cd213d24
Your Actual Name <Your Actual Email>
2017-10-04 21:07:39 -0500

    mvp upload

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Test Five</h1>
+     <div>
+       <h1 style={{textAlign: 'center'}}>
+        Test Five</h1>
+        <div>Test five.1 </div>
       </div>);
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,18 @@
-/*
-    ./webpack.config.js
-*/
 const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
-  filename: 'index.html',
-  inject: 'body'
-})
-// changed but not sure it's right
-module.exports = {
+
+const config = {
   entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: '../dist/index_bundle.js'
+    filename: 'index_bundle.js'
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-},
-// add this line
-plugins: [HtmlWebpackPluginConfig]
+  }
 }
+
+module.exports = config;
