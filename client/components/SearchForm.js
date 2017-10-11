efb7864d33b13426a533fee23b37c22a4d5d6466
sara heymann <saraheymann@saras-MacBook-Pro.local>
2017-10-11 18:20:09 -0500

    changed search form

--- client/components/SearchForm.js
+++ client/components/SearchForm.js
@@ -5,17 +5,10 @@ class SearchForm extends Component{
         return(
             <div>
                 <form name="type_search" action="/search" method="post">  
-                    <label>
-                        Search for a type of artwork:
-                    <select name="type" value="type">
-                        <option name="type" value="mural">Mural</option>
-                        <option name="type" value="sculpture">Sculpture</option>
-                        <option name="type" value="statue">Statue</option>
-                        <option name="type" value="graffiti">Graffiti</option>
-                        <option name="type" value="performance">Performance</option>
-                        <option name="type" value="other">Other</option>
-                        </select>
-                    </label>
+                <label>
+                Type of Artwork:
+                    <input type="text" name="type" placeholder="Mural, Sculpture, Statue, Graffiti, Performance, Other"/>
+                </label>
                     <input type="submit" name="Submit" id= "submitBox"/>
                 </form>
             </div>
