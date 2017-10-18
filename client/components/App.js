41a3f055380b2258feb0da9e90b80a400022c128
intaglioplate312 <helenelizabeth@me.com>
2017-10-18 12:14:42 -0500

    NavBar Bootstrap Gridded

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WholeNavBar from './WholeNavBar';
+// import WholeNavBar from './WholeNavBar';
 import AddArtworkForm from './uncontrolledForm'
 import SearchForm from './SearchForm'
-// import NavHeader from './NavHeader'
+import NavHeader from './NavHeader'
 // import About from './About'
 
 
@@ -11,8 +11,8 @@ export default class App extends React.Component {
   render() {
        return (
       <div className="App"> 
-         <WholeNavBar/>
-         {/* <NavHeader/> */}
+         {/* <WholeNavBar/> */}
+         <NavHeader/> 
          {/* <About/> */}
          <AddArtworkForm/>
          <SearchForm/>
